ba3d7bb740c28ce58c822e28016a989e80d4e7b8
Dsolm <dsola2405@gmail.com>
2024-12-03 11:23:16 +0100

    Skip covered voxels

--- src/mundo.rs
+++ src/mundo.rs
@@ -2,7 +2,8 @@ pub struct Mundo {
     bloques: Vec<u8>
 }
 
-pub const MIDA: usize = 64;
+pub const MIDA: usize = 255;
+pub const MIDA_U8: u8 = MIDA as u8;
 
 pub const VERTICES_CUADRADO: [f32; 36*3] = [
 	-1.0,-1.0,-1.0, // triangle 1 : begin
@@ -70,6 +71,21 @@ impl Mundo {
         }
     }
 
+
+    fn is_air(&self, x: u8, y:u8, z:u8) -> bool {
+        self.get(x, y, z) == 0
+    }
+
+    fn touches_air(&self, x: u8, y: u8, z:u8) -> bool {
+
+        (x < MIDA_U8-1 && self.is_air(x+1,y,z)) || 
+            (x > 0 && self.is_air(x-1,y,z)) || 
+            (y < MIDA_U8-1 && self.is_air(x,y+1,z)) || 
+            (y > 0 && self.is_air(x,y-1,z)) || 
+            (z < MIDA_U8-1 && self.is_air(x,y,z+1)) || 
+            (z > 0 && self.is_air(x,y,z-1))
+    }
+
     pub fn to_vertex(&self) -> Vec<f32> {
         let mut vertices = vec![];
 
@@ -79,16 +95,16 @@ impl Mundo {
                     if self.get(x,y,z) == 0 {
                         continue;
                     }
-                    
-                    for i in 0..36 {
-                        vertices.push(VERTICES_CUADRADO[i*3] + x as f32);
-                        vertices.push(VERTICES_CUADRADO[i*3 + 1] + y as f32);
-                        vertices.push(VERTICES_CUADRADO[i*3 + 2] + z as f32);
+                    if self.touches_air(x,y,z) {
+                        for i in 0..36 {
+                            vertices.push(VERTICES_CUADRADO[i*3] + x as f32);
+                            vertices.push(VERTICES_CUADRADO[i*3 + 1] + y as f32);
+                            vertices.push(VERTICES_CUADRADO[i*3 + 2] + z as f32);
+                        }
                     }
                 }
             }
         }
-
         vertices
     }
 
